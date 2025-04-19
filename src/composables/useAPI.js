98f547cae14d1167eb352c82f277f04e99d862b8
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-04-19 01:12:18 -0500

    card details finished

--- src/composables/useAPI.js
+++ src/composables/useAPI.js
@@ -7,6 +7,7 @@ const instance = axios.create({
 
 const employees = ref([])
 const loading = ref(false)
+const currentEmployee = ref(null) 
 
 export default function useAPI() {
 
@@ -19,5 +20,10 @@ export default function useAPI() {
         loading.value = false 
     }
 
-    return {instance, employees, getEmployees, loading} 
+    const fetchEmployee = async (id) => {
+        const response = await instance.get(`api/employees/fetch/${id}`)
+        currentEmployee.value = response.data
+    }
+
+    return {instance, employees, getEmployees, loading, fetchEmployee, currentEmployee} 
 }
